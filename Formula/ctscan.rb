ec5073dd301ceb3ae0d01c5e7c797adde7f100b5
Onur Tellioglu <94357256+onur-tellioglu@users.noreply.github.com>
2026-04-04 01:05:37 +0200

    ctscan 0.2.0

--- Formula/ctscan.rb
+++ Formula/ctscan.rb
@@ -1,8 +1,8 @@
 class Ctscan < Formula
   desc "macOS system health scanner"
   homepage "https://github.com/onur-tellioglu/ctscan"
-  url "https://github.com/onur-tellioglu/ctscan/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "9591a36b5f08836c7aeb533f49539b482b37ad503a3e1d8a42ab81c94995b0a7"
+  url "https://github.com/onur-tellioglu/ctscan/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0dcbee9736ee4e73209c615a4554d4b10293643c2b2d9369b7655bf31121e9da"
   license "MIT"
 
   def install
